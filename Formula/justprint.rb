2a918415f33a0660e807c71272127733da0ebeae
方晓超 <fxc@fangxiaochaodeMacBook-Pro.local>
2021-10-26 16:55:10 +0800

    feat: update ruby script

---
+++ Formula/justprint.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Justprint < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/franticn/homebrew-just4fun/blob/main/justprint_0.0.1.tar.gz?raw=true"
+  sha256 "dae8b8e72b47cf98fa879c63b99a070eafdbf7f8f60f902bb9961ec5c11117f0"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system echo "开始安装"
+    system ls
+    system pwd
+    bin.install "justprint"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test justprint`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
